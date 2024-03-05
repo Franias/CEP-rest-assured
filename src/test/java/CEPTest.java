6cc6ec837fe65d6a9422d8314f7ab5adcaba1b59
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2024-03-05 18:49:21 -0300

    .gitignore

---
+++ src/test/java/CEPTest.java
@@ -0,0 +1,52 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CEPTest {
+    private RequestSpecification requestSpec;
+
+    @BeforeEach
+    public void createRequestSpecification() {
+
+      requestSpec = new RequestSpecBuilder().
+          setBaseUri("https://viacep.com.br").
+          build();
+    }
+
+    @Test
+    public void requestCEPValido_checkResponseCode_expect200() {
+        String cepValido = "01001000";
+
+      given().
+          spec(requestSpec).
+          when().
+          get("ws/" + cepValido +"/json/").
+          then().
+          assertThat().
+          statusCode(200).
+          contentType(equalTo("application/json; charset=utf-8")).
+          body("localidade", equalTo("São Paulo"));;
+
+    }
+
+    @Test
+    public void requestCEPInvalido_checkResponseCode_expect200() {
+      String cepInvalido = "";
+
+      given().
+          spec(requestSpec).
+          when().
+          get("ws/" + cepInvalido +"/json/").
+          then().
+          assertThat().
+          statusCode(400);
+
+    }
+
+
+
+}
